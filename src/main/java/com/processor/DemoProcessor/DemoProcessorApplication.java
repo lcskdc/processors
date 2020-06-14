0d2f327ce31e0aeccff018f253143515e491fd44
Lucas Pacheco Oliveira <esc_loliveira@uolinc.com>
2020-06-14 11:57:41 -0300

    Commit inicial

---
+++ src/main/java/com/processor/DemoProcessor/DemoProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.processor.DemoProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProcessorApplication.class, args);
+	}
+
+}
